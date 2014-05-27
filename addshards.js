6a3e6cda56decc1147bf741880a62ed3874f7081
Hiroyoshi Kurohara <hkurohar@insight-tec.co.jp>
2014-05-27 15:28:30 +0900

    usage of range tag

--- addshards.js
+++ addshards.js
@@ -1,4 +1,11 @@
 sh.addShard("localhost:27028");
 sh.addShard("localhost:27038");
 sh.enableSharding("wiperdog");
+// sh.shardCollection("wiperdog.SysCpu", { "hostId": "hashed" });
+// sh.shardCollection("wiperdog.SysCpu", { "hostId": 1 });
+sh.shardCollection("wiperdog.SysCpu", { "mgroup": 1 });
+sh.addShardTag("shard0000", "GRPA");
+sh.addShardTag("shard0001", "GRPB");
+sh.addTagRange("wiperdog.SysCpu", { "mgroup": MinKey }, {"mgroup": "A"}, "GRPA");
+sh.addTagRange("wiperdog.SysCpu", { "mgroup": "B" }, {"mgroup": MaxKey}, "GRPB");
 
